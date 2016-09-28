9c3ee8715443af58564ddec03ef6c91bc2f0d4ff
ThivyaK <thivya_kanagendran@yahoo.com>
2016-09-28 17:47:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 
 //change the value of main-text div
 var element = document.getElementById('main-text');
-element.innerHtml('New Value changed !');
+element.innerHtml = 'New value !';
 
 //Move the image
