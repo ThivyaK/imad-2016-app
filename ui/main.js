0b37940101d86eba726fe0ea6b58f3faa430cd34
ThivyaK <thivya_kanagendran@yahoo.com>
2016-09-28 18:02:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,10 @@ element.innerHtml = 'New value !';
 
 var imgElement = document.getElementById('coder');
 var marginLeft = 0;
-
+function moveRight() {
 marginLeft = marginLeft + 10;
 coder.style.marginLeft = marginLeft + 'px';
+}
 imgElement.onclick = function() {
     var interval = setInterval(moveRight,100);
 }
