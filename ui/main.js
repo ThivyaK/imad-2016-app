5b76154c19bea4b83c8c6dc77988f122843b44d3
ThivyaK <thivya_kanagendran@yahoo.com>
2016-09-28 18:08:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,16 +3,16 @@ console.log('Loaded!');
 //change the value of main-text div
 var element = document.getElementById('main-text');
 
-element.innerHtml = 'New value !';
+element.innerHTML = 'New value !';
 
 //Move the image
 
 var imgElement = document.getElementById('coder');
 var marginLeft = 0;
 function moveRight() {
-marginLeft = marginLeft + 10;
+marginLeft = marginLeft + 1;
 coder.style.marginLeft = marginLeft + 'px';
 }
 imgElement.onclick = function() {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,50);
 }
