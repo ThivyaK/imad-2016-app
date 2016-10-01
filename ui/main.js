cc76d4b52e8e9606a47963bb0d7e66c5f2e32a59
ThivyaK <thivya_kanagendran@yahoo.com>
2016-10-01 18:14:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,8 +60,8 @@ submit.onclick = function() {
   //capture the list of names and render it as a list
   var names = [name1,name2,name3]
   var list = "";
-  for(i=0,i<names.length,i++){
-      list+= '<li>' + names + '</li>';
+  for(var i=0;i<names.length;i++){
+      list+= '<li>' + names[i] + '</li>';
   }
   
   var ul = document.getElementById('ul-list');
