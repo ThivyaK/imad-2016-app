d8f67ba3836cd0738496b0a230c6f18692b87c43
ThivyaK <thivya_kanagendran@yahoo.com>
2016-10-03 21:02:39 +0530

    [imad-console] Updates ui/articleName.js

--- ui/articleName.js
+++ ui/articleName.js
@@ -18,7 +18,7 @@ submitcomment.onclick = function() {
               comments = JSON.parse(comments);
               var list="";
               for(var i=0;i<comments.length;i++) {
-                  list += '<li>' + comments[i] + '</li>';
+                  list += '<li>' + comments[i] + '<hr/>' + '</li>';
               }
               
               var ulcomment = document.getElementById('comment-list');
