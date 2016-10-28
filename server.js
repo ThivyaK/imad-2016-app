41ab95c5c5163504ef5659037bf1a266121d4538
ThivyaK <thivya_kanagendran@yahoo.com>
2016-10-28 17:34:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ var htmlTemplate = `
                 ${heading}
             </h3>
             <div>
-                ${date}
+                ${date.toDateString()}
             </div>
             <div>
                 ${content}
@@ -156,11 +156,20 @@ app.get('/articleName',function(req,res){ //URL:/articleName/?comment=xxx
    res.send(JSON.stringify(comments));
 });
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
 //articleName==='article-one'
 //articles[articleName]=== {} content object for article-one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result){
+    if(err){
+        res.status(500).send(err.toString());
+    }else
+    if(result.rows.length===0){
+        res.status(404).send('Article not found');
+    }else {
+        var articledata = result.rows[0];
+        res.send(createTemplate(articledata));
+      }
+});
 });
 
 
