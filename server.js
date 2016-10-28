67d3a6ecb2d8585b98a2fb4e29e6c7351e1c9fee
ThivyaK <thivya_kanagendran@yahoo.com>
2016-10-28 18:08:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,61 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles= {
-   'article-one':{
-title:'Article-One | Thivya Kanagendran',
-date:'Sep5 , 2016',
-heading:'Article One',
-content:`
-	<p>
-   		This is the content for my first article.This is the content for my first article.This is the content for my first articl. This is the content for my first article.This is the content for my first article.This is the content for my first article.
-	</p>
-	<p>
-   		This is the content for my first article.This is the content for my first article.This is the content for my first articl. This is the content for my first article.This is the content for my first article.This is the content for my first article.
-	</p>
-	<p>
-   		This is the content for my first article.This is the content for my first article.This is the content for my first articl. This is the content for my first article.This is the content for my first article.This is the content for my first article.
-	</p>`
-
-	
-
-},
-
-  'article-two':{
-title:'Article-Two| Thivya Kanagendran',
-date:'Sep10 , 2016',
-heading:'Article Two',
-content:`
-	<p>
-   		This is the content for my second article.
-	</p>
-	<p>
-   		This is the content for my second article.
-	</p>
-	<p>
-   		This is the content for my second article.
-	</p>`
-
-},
-
-   'article-three':{
-title:'Article-Three| Thivya Kanagendran',
-date:'Sep15 , 2016',
-heading:'Article Three',
-content:`
-	<p>
-   		This is the content for my Third article.
-	</p>
-	<p>
-   		This is the content for my Third article.
-	</p>
-	<p>
-   		This is the content for my Third article. 
-	</p>`
-    }
-
-};
-
 function createTemplate(data) {
 var title = data.title;
 var heading = data.heading;
