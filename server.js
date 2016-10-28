32ae1096a9f48806add8e7328a31cbfb225eca08
ThivyaK <thivya_kanagendran@yahoo.com>
2016-10-28 15:57:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    username:'thivyak',
+    Database:'thivyak',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD,
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -60,8 +69,6 @@ content:`
 
 };
 
-
-
 function createTemplate(data) {
 var title = data.title;
 var heading = data.heading;
@@ -120,6 +127,19 @@ app.get('/counter',function(req,res) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    //make a select request
+    //return with the response results
+    pool.query('SELECT * FROM Test', function(err,result){
+        if(err){
+            res.status('500').send(err.toString());
+        } else {
+            res.send(JSON.Stringify(result));
+          }
+    });
 });
 
 var names = [];
