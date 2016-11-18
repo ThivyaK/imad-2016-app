35a7a42d36e875d5de43dd3bb38683805a94ccb2
ThivyaK <thivya_kanagendran@yahoo.com>
2016-11-19 00:29:29 +0530

    [imad-console] Updates ui/articleName.js

--- ui/articleName.js
+++ ui/articleName.js
@@ -34,3 +34,38 @@ submitcomment.onclick = function() {
         request.send(comment);
         
 };
+//Submit name code
+var submitbtn = document.getElementById('submit');
+submitbtn.onclick = function() {
+      // Make a request to the server and send the name
+       var request = new XMLHttpRequest();
+      
+      //capture the response & store it in a variable
+      request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE )
+        //Take some action
+        {
+            if(request.status === 200)
+            {
+              //capture the list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = "";
+              for(var i=0; i<names.length; i++)
+              {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              
+              var ul = document.getElementById('ul-list');
+              ul.innerHTML = list;
+            }
+            
+            //Not done yet
+        }
+      }
+         //Make the request
+          var nameInput = document.getElementById('name');
+          var name = nameInput.value;
+          request.open('GET','http://thivyak.imad.hasura-app.io/submit-name/?name=' + name,true);
+          request.send(null);
+};
