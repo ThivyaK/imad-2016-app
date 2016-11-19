30ed2f11d6ebb59c7dcad09c2f5f0750192a6079
ThivyaK <thivya_kanagendran@yahoo.com>
2016-11-20 02:08:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -80,7 +80,6 @@ var submitbtn = document.getElementById('submit');
           var password = document.getElementById('password').value;
           console.log(username);
           console.log(password);
-          var name = nameInput.value;
           request.open('POST','http://thivyak.imad.hasura-app.io/login',true);
           request.setRequestHeader('Content-Type','application/json');
           request.send(JSON.stringify({username:username,password:password}));
