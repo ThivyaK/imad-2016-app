f9bebdd4039be5f9a9d82677c0b7e1e08980f5f8
ThivyaK <thivya_kanagendran@yahoo.com>
2016-11-20 02:07:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,38 +20,38 @@
 
 //counter code
 
-var button = document.getElementById('counterbtn');
-button.onclick = function() {
-  // create a request object
+// var button = document.getElementById('counterbtn');
+// button.onclick = function() {
+//   // create a request object
   
-  var request = new XMLHttpRequest();
+//   var request = new XMLHttpRequest();
   
-  //capture the response & store it in a variable
-  request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE )
-    //Take some action
-    {
-        if(request.status === 200)
-        {
-            var counterResponse = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counterResponse.toString();
-        }
+//   //capture the response & store it in a variable
+//   request.onreadystatechange = function() {
+//     if(request.readyState === XMLHttpRequest.DONE )
+//     //Take some action
+//     {
+//         if(request.status === 200)
+//         {
+//             var counterResponse = request.responseText;
+//             var span = document.getElementById('count');
+//             span.innerHTML = counterResponse.toString();
+//         }
         
-        //Not done yet
-    }
+//         //Not done yet
+//     }
     
-   }
+//   }
 
-  //Make the request to the counter endpoint
-  request.open('GET','http://thivyak.imad.hasura-app.io/counter',true);
-  request.send(null);
-};
+//   //Make the request to the counter endpoint
+//   request.open('GET','http://thivyak.imad.hasura-app.io/counter',true);
+//   request.send(null);
+// };
 
 
-//Submit name code
+//Submit username/password to login
 var submitbtn = document.getElementById('submit');
-submitbtn.onclick = function() {
+     submitbtn.onclick = function() {
       // Make a request to the server and send the name
        var request = new XMLHttpRequest();
       
@@ -63,26 +63,27 @@ submitbtn.onclick = function() {
             if(request.status === 200)
             {
               //capture the list of names and render it as a list
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = "";
-              for(var i=0; i<names.length; i++)
-              {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              
-              var ul = document.getElementById('ul-list');
-              ul.innerHTML = list;
+              console.log('user logged in');
+              alert('Logged in successfully');
             }
-            
-            //Not done yet
+              else if(request.status === 403){
+                  alert('Username/password is incorrect');
+              }
+              else if(request.status === 500){
+                  alert('Something went wrong on the server');
+              }
+           
         }
       }
          //Make the request
-          var nameInput = document.getElementById('name');
+          var username = document.getElementById('username').value;
+          var password = document.getElementById('password').value;
+          console.log(username);
+          console.log(password);
           var name = nameInput.value;
-          request.open('GET','http://thivyak.imad.hasura-app.io/submit-name/?name=' + name,true);
-          request.send(null);
+          request.open('POST','http://thivyak.imad.hasura-app.io/login',true);
+          request.setRequestHeader('Content-Type','application/json');
+          request.send(JSON.stringify({username:username,password:password}));
 };
 
 
