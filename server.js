2e8eca47d3c1db82e8e367576d10fc93ea18c6f8
ThivyaK <thivya_kanagendran@yahoo.com>
2016-11-20 03:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ app.post('/login',function(req,res){
                 {
                     //set session
                     
-                    req.session.auth = result.rows[0].id;
+                     req.session.auth = {userId: result.rows[0].id};
                     
                       res.send('Right Credentials!');
                 }
